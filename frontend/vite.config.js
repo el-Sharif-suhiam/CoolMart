aa6521cd8c27aaacde2ad0889bd8ad7c352a2cad
el-sharif-suhiam <zero_robot77@protonmail.com>
2025-01-08 00:21:09 +0200

    nothing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,8 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+//
+
 dotenv.config({ path: resolve(__dirname, "../.env") });
 
 export default defineConfig({
